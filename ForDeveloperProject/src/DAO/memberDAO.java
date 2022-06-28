e3ec98bbc63bafd8507b78d8905e05e047333725
wormsJJG <jjr66461837@gmail.com>
2022-06-28 14:36:08 +0900

    Setting memberDAO

--- ForDeveloperProject/src/DAO/memberDAO.java
+++ ForDeveloperProject/src/DAO/memberDAO.java
@@ -1,5 +1,12 @@
 package DAO;
 
-public class memberDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class memberDAO {
+	Connection conn = null;
+	PreparedStatement stmt = null;
+	ResultSet rs = null;
+	String sql = "";
 }
